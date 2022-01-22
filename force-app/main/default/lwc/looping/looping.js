fd6fc2b919b239cad5b4cb1a2e5b5ce5b6dd3190
navneet <navneetjain10@gmail.com>
2022-01-23 00:53:07 +0530

    iterator and foreach on object array

--- force-app/main/default/lwc/looping/looping.js
+++ force-app/main/default/lwc/looping/looping.js
@@ -2,4 +2,22 @@ import { LightningElement } from 'lwc';
 
 export default class Looping extends LightningElement {
     carList = ["Audi","BMW","Jaguar"]
+
+    ceoList = [
+        {
+            id:1,
+            company:"HCL",
+            name:"Roshni"
+        },
+        {
+            id:2,
+            company:"Facebook",
+            name:"Mark"
+        },
+        {
+            id:3,
+            company:"Google",
+            name:"Sundar"
+        }
+    ]
 }
