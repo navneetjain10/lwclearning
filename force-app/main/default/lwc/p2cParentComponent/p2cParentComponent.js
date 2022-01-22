4c85f79c35c6e898c2fa17d974b7b4718d2daa33
navneet <navneetjain10@gmail.com>
2022-01-23 01:46:05 +0530

    parent to child non primitive data pass

--- force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
+++ force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
@@ -1,3 +1,27 @@
 import { LightningElement } from 'lwc';
 
-export default class P2cParentComponent extends LightningElement {}
+export default class P2cParentComponent extends LightningElement {
+carouselData =[
+    {
+        src:"https://www.lightningdesignsystem.com/assets/images/carousel/carousel-01.jpg",
+        header:"First Card",
+        description:"First card description.",
+        alternativetext:"First card accessible description.",
+        href:"javascript:void(0);"
+    },
+    {
+        src:"https://www.lightningdesignsystem.com/assets/images/carousel/carousel-02.jpg",
+        header:"Second Card",
+        description:"Second card description.",
+        alternativetext:"Second card accessible description.",
+        href:"javascript:void(0);"
+    },
+    {
+        src:"https://www.lightningdesignsystem.com/assets/images/carousel/carousel-03.jpg",
+        header:"Third Card",
+        description:"Third card description.",
+        alternativetext:"Third card accessible description.",
+        href:"javascript:void(0);"
+    }
+]
+}
