cd27c3023acb97fd8af9134ca2f1ef90c0162d7e
navneet <navneetjain10@gmail.com>
2022-01-23 18:08:12 +0530

    child to parent event with data

--- force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
+++ force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
@@ -2,7 +2,9 @@ import { LightningElement } from 'lwc';
 
 export default class C2pModalComponent extends LightningElement {
     closeHandler() {
-        const closeEvent = new CustomEvent('close')
+        const closeEvent = new CustomEvent('close', {
+            detail: "Modal closed sucessfully!"
+        })
         this.dispatchEvent(closeEvent)
     }
 }
