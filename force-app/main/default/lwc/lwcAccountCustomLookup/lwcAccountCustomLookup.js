f2214222d364d03fdc1d5707c8d7b2719c461fdc
navneet <navneetjain10@gmail.com>
2022-02-23 23:17:31 +0530

    contact lookup added in account

---
+++ force-app/main/default/lwc/lwcAccountCustomLookup/lwcAccountCustomLookup.js
@@ -0,0 +1,63 @@
+import { LightningElement, track, wire } from 'lwc'
+import getCustomLookupAccount from '@salesforce/apex/lwcApexAccountController.getCustomLookupAccount'
+
+export default class LwcAccountCustomLookup extends LightningElement {
+    @track accountName = '';
+    @track accountList = [];
+    @track objectApiName = 'Account';
+    @track accountId;
+    @track isShow = false;
+    @track messageResult = false;
+    @track isShowResult = true;
+    @track showSearchedValues = false;
+
+    @wire(getCustomLookupAccount, { accountName: '$accountName' })
+    retrieveAccounts({ data, error }) {
+        this.messageResult = false;
+        if (data) {
+            console.log('data## ' + data.length);
+            if (data.length > 0 && this.isShowResult) {
+                this.accountList = data;
+                this.showSearchedValues = true;
+                this.messageResult = false;
+            } else if (data.length == 0) {
+                this.accountList = [];
+                this.showSearchedValues = false;
+                if (this.accountName != '') {
+                    this.messageResult = true;
+                }
+            } else if (error) {
+                this.accountId = '';
+                this.accountName = '';
+                this.accountList = [];
+                this.showSearchedValues = false;
+                this.messageResult = true;
+            }
+
+        }
+    }
+
+    searchHandleClick(event) {
+        this.isShowResult = true;
+        this.messageResult = false;
+    }
+
+    searchHandleKeyChange(event) {
+        this.messageResult = false;
+        this.accountName = event.target.value;
+    }
+
+    parentHandleAction(event) {
+        this.showSearchedValues = false;
+        this.isShowResult = false;
+        this.messageResult = false;
+        //Set the parent calendar id
+        this.accountId = event.target.dataset.value;
+        //Set the parent calendar label
+        this.accountName = event.target.dataset.label;
+        console.log('accountId::' + this.accountId);
+        const selectedEvent = new CustomEvent('selected', { detail: this.accountId });
+        // Dispatches the event.
+        this.dispatchEvent(selectedEvent);
+    }
+}
